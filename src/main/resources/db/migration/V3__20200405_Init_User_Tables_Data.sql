74234c0ba29b2646ba19073eae720f31e82c4f3f
Pranav Patel <pranavpatel986@gmail.com>
2022-11-20 00:54:33 +0530

    d

--- src/main/resources/db/migration/V3__20200405_Init_User_Tables_Data.sql
+++ src/main/resources/db/migration/V3__20200405_Init_User_Tables_Data.sql
@@ -27,12 +27,12 @@ CREATE TABLE user
 -- Dumping data for table user
 --
 
-LOCK TABLES USER WRITE;
+LOCK TABLES user WRITE;
 INSERT INTO user
-VALUES (1, 'uzumaki_naruto@konohagakure.co.jp', '', 'Uzumaki', 'Naruto',
-        '$2a$12$DWCryEwHwbTYCegib92tk.VST.GG1i2WAqfaSwyMdxX0cl0eBeSve', '5551112345', 'User', 1, 1),
-       (2, 'uchiha_sasuke@konohagakure.co.jp', '', 'Uchiha', 'Sasuke',
-        '$2a$12$hZR7pcSf0JU/OTXR3TOyuu8r6C6n.JZE8Ei47E4LZs1t0Aq1AO6oC',
+VALUES (1, 'patelpranav986@gmail.com', '', 'Pranav', 'Patel',
+        '$2a$12$LtbYtEzOBdnsiD/E9Wtj2OfisOTyGV1PANy5rWMH7dZOBoN/vKgbu', '5551112345', 'User', 1, 1),
+       (2, 'pranavpatel986@gmail.com', '', 'Pranav', 'Patel',
+        '$2a$12$LtbYtEzOBdnsiD/E9Wtj2OfisOTyGV1PANy5rWMH7dZOBoN/vKgbu',
         '1111111111', 'Admin', 2, 2);
 UNLOCK TABLES;
 
